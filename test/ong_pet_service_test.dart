bdffa0bdee363aee88044445855c8bd57a91187b
Isaac César <isac_contest@hotmail.com>
2022-06-24 11:35:48 -0300

    testes da instancia de dogs realizado

--- test/ong_pet_service_test.dart
+++ test/ong_pet_service_test.dart
@@ -2,8 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ong_pet_desafio/domain/service/ong_pet_service.dart';
 
 
-void main() async {
-  final service = OngPetService();
-  final dogs = await service.getAllDogs();
-  print(dogs[0].name);
+void main() {
+
+  test('deve retornar todos os cachorros', () async {
+    final service = OngPetService();
+    final dogs = await service.getAllDogs();
+    expect(dogs[0].name, 'Affenpinscher');
+  });
 }
