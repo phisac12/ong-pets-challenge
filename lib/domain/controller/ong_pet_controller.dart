b815621d9112260ce1239287a956d507035af3e6
Isaac César <isac_contest@hotmail.com>
2022-06-25 21:12:49 -0300

    ajustes no controller | ajustes na view

--- lib/domain/controller/ong_pet_controller.dart
+++ lib/domain/controller/ong_pet_controller.dart
@@ -1,14 +1,18 @@
-
+import 'package:flutter/material.dart';
 import 'package:ong_pet_desafio/domain/model/ong_pet_model.dart';
-import 'package:ong_pet_desafio/domain/services/dio_client.dart';
 import 'package:ong_pet_desafio/domain/services/ong_pet_service.dart';
 
-class OngPetController {
-  final _service = OngPetService(DioClient());
+class OngPetController extends ChangeNotifier{
+
+  final OngPetService _service;
+
+  OngPetController(this._service);
+
   var pets = <OngPetModel>[];
 
   Future<void> fetchAllPets() async {
     pets = await _service.getAllDogs();
+    notifyListeners();
   }
 
 }
--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:ong_pet_desafio/domain/controller/ong_pet_controller.dart';
+import 'package:ong_pet_desafio/domain/services/dio_client.dart';
+import 'package:ong_pet_desafio/domain/services/ong_pet_service.dart';
 import 'package:ong_pet_desafio/ui/style/app_colors.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = OngPetController(OngPetService(DioClient()));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.deepPurpleAccent.shade200,
       appBar: AppBar(
         backgroundColor: AppColors.secondaryColor,
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('Ong Pets'),
-            Image.asset('assets/images/logo.png', width: 50,),
+            const SizedBox(
+              width: 5,
+            ),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 65,
+            ),
           ],
         ),
-        centerTitle: true,
+      ),
+      body: AnimatedBuilder(
+        animation: controller,
+        builder: (context, widget) {
+         return ListView.builder(
+              itemCount: controller.pets.length,
+              itemBuilder: (context, index) {
+                final dog = controller.pets[index];
+                return ListTile(
+                  title: Text(dog.name),
+                );
+              });
+        },
       ),
     );
   }
